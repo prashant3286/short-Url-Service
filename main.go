26ee432c086d2fd235c2ba003c1f8777eb287e87
Prashant Bhandari <cu.17bcs3286@gmail.com>
2023-05-18 13:04:42 +0545

    main function for shorten url

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/prashant3286/short-url-service/handlers"
+)
+
+func main() {
+	http.HandleFunc("/shorten", handlers.ShortenURL)
+	http.HandleFunc("/", handlers.RedirectURL)
+
+	log.Println("Server started on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
